038d6c26efdbed75efe041fa701f96f41898130f
Wing <114337707+wing-0902@users.noreply.github.com>
2026-04-29 10:07:57 +0000

    これはすごい

---
+++ src/lib/components/CurrentTime.svelte
@@ -0,0 +1,22 @@
+<script lang='ts'>
+	let now = $state(new Date());
+
+	$effect(() => {
+		const interval = setInterval(() => {
+			now = new Date();
+		}, 1000);
+
+		return () => clearInterval(interval);
+	});
+
+	// フォーマット済みの文字列（リアクティブに更新される）
+	const timeString = $derived(now.toLocaleTimeString());
+</script>
+
+<p class='timer'>{timeString}</p>
+
+<style lang="scss">
+    .timer {
+        font-size: 30px;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,16 @@
   import favicon from '$lib/assets/favicon.svg';
 
   let { children } = $props();
+
+  import '$lib/global.scss';
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
+
+<header>
+  <h1>地理<small>授業時間</small>探求<small>App</small></h1>
+</header>
 
 {@render children()}
