e52848642223afad50cbb835dd1768a0af4957fe
Wing <114337707+wing-0902@users.noreply.github.com>
2026-04-29 13:20:40 +0000

    meta data

--- src/lib/components/HistoryGraph.svelte
+++ src/lib/components/HistoryGraph.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { historyStore } from '$lib/utils/syncLocal.svelte';
   import { getValueToName } from '$lib/utils/getValueToName';
+  import { goto } from '$app/navigation';
 
   // 相対時間を計算するなどの補助関数
   const formatTime = (ts: number) => new Date(ts).toLocaleTimeString();
@@ -16,11 +17,19 @@
   {/each}
 </ul>
 
+<button class='graph' onclick={() => goto('/graph/')}>
+  グラフを表示
+</button>
+
 <style lang="scss">
   .list {
     font-size: 20px;
 
     list-style-type: none;
     padding-left: 0;
+  }
+
+  .graph {
+    width: 270px;
   }
 </style>
